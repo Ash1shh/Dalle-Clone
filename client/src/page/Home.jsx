cd35a71190e7c251e531a39d5f8fe63bd117ed39
Ash1shh <Ashishchandra8328@gmail.com>
2023-04-01 00:22:42 +0530

    :art: ichitram

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://ichitram.onrender.com/api/v1/post', {
+      const response = await fetch('https://ichitram.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
